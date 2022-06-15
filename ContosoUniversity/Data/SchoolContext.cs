4a9a8be4a9cbad284e758d7361a9412550869140
Stefan Larsson <stefan.larsson@nordlo.it>
2022-06-15 11:48:21 +0200

    Tutorial complete

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,36 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable(nameof(Course))
+                .HasMany(c => c.Instructors)
+                .WithMany(i => i.Courses);
+            modelBuilder.Entity<Student>().ToTable(nameof(Student));
+            modelBuilder.Entity<Instructor>().ToTable(nameof(Instructor));
+
+            modelBuilder.Entity<Department>()
+                .Property<byte[]>("ConcurrencyToken")
+                .IsRowVersion();
+        }
+
+    }
+}
---
+++ ContosoUniversity/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var myMaxModelBindingCollectionSize = Convert.ToInt32(
+           builder.Configuration["MyMaxModelBindingCollectionSize"] ?? "100");
+
+builder.Services.Configure<MvcOptions>(options =>
+       options.MaxModelBindingCollectionSize = myMaxModelBindingCollectionSize);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolContext>();
+    //context.Database.EnsureCreated(); // EnsureCreated doesn't create a migrations history table and so can't be used with
+                                        // migrations. It's designed for testing or rapid prototyping where the database is
+                                        // dropped and re-created frequently.
+    DbInitializer.Initialize(context);
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
